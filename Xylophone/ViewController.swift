90894bf3980eccc0b9a1d316dffacc8cc40bcb9d
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-08-03 14:07:29 +0600

    Instead of using individual functions per button here I learnt to link multiple button with one function. Then from reading the button title from sender, I tackled the problem of playing different sound for different button

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -16,40 +16,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func onCKeyPress(_ sender: UIButton) {
-        print("C got pressed")
-        playSound(fileName: "C")
+    @IBAction func onKeyPressed(_ sender: UIButton) {
+        playSound(fileName: sender.currentTitle!)
     }
-    @IBAction func onDKeyPress(_ sender: UIButton) {
-        print("D got pressed")
-        playSound(fileName: "D")
-    }
-    
-    @IBAction func onEKeyPress(_ sender: UIButton) {
-        print("E got pressed")
-        playSound(fileName: "E")
-    }
-    
-    @IBAction func onFKeyPress(_ sender: UIButton) {
-        print("F got pressed")
-        playSound(fileName: "F")
-    }
-    
-    @IBAction func onGKeyPress(_ sender: UIButton) {
-        print("G got pressed")
-        playSound(fileName: "G")
-    }
-    
-    @IBAction func onAKeyPress(_ sender: UIButton) {
-        print("A got pressed")
-        playSound(fileName: "A")
-    }
-    
-    @IBAction func onBKeyPress(_ sender: UIButton) {
-        print("B got pressed")
-        playSound(fileName: "B")
-    }
-    
+
     /// Audio Functionality
     var player: AVAudioPlayer?
     
